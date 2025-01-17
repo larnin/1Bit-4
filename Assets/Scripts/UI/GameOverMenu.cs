d88fc52b0272e8bcfab88fbc28c71cf958fa4495
Nicolas <nicolas-donald.laurent@laposte.net>
2025-01-17 14:06:45 +0100

    GameOver WIP

---
+++ Assets/Scripts/UI/GameOverMenu.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using DG.Tweening;
+
+public class GameOverMenu : MonoBehaviour
+{
+    [SerializeField] string m_menuName;
+    [SerializeField] float m_appearOffset;
+    [SerializeField] float m_appearDuration;
+    [SerializeField] Ease m_appearCurve;
+
+    bool m_selected = false;
+
+    public void OnContinue()
+    {
+        if (m_selected)
+            return;
+
+        m_selected = true;
+
+        SceneSystem.changeScene(m_menuName);
+    }
+
+    private void Awake()
+    {
+        var obj = transform.Find("Pivot");
+        if(obj != null)
+        {
+            var target = obj.localPosition;
+            var start = obj.localPosition + new Vector3(0, m_appearOffset, 0);
+
+            obj.localPosition = start;
+
+            obj.DOLocalMove(target, m_appearDuration).SetEase(m_appearCurve);
+        }
+    }
+}
--- Assets/Scripts/World/Buildings/BuildingTower.cs
+++ Assets/Scripts/World/Buildings/BuildingTower.cs
@@ -16,6 +16,7 @@ public class BuildingTower : BuildingBase
     {
         base.Awake();
         m_subscriberList.Add(new Event<BuildSelectionDetailCommonEvent>.LocalSubscriber(BuildCommon, gameObject));
+        m_subscriberList.Add(new Event<DeathEvent>.LocalSubscriber(OnDeath, gameObject));
         m_subscriberList.Subscribe();   
     }
 
@@ -45,5 +46,13 @@ public class BuildingTower : BuildingBase
         DisplayGenericInfos(e.container);
 
         UIElementData.Create<UIElementLabelAndText>(e.container).SetLabel("Power").SetText(m_powerGeneration.ToString());
+    }
+
+    void OnDeath(DeathEvent e)
+    {
+        if (MenuSystem.instance == null)
+            return;
+
+        MenuSystem.instance.OpenMenu<GameOverMenu>("GameOver");
     }
 }
