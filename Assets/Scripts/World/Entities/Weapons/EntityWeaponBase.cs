20ffb235a35b10dcb9bffb701e780300e311c3f0
Nico <nicolas-donald.laurent@laposte.net>
2025-02-18 23:23:46 +0100

    Profiling entities

--- Assets/Scripts/World/Entities/Weapons/EntityWeaponBase.cs
+++ Assets/Scripts/World/Entities/Weapons/EntityWeaponBase.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Unity.Profiling;
 using UnityEngine;
 
 public abstract class EntityWeaponBase : MonoBehaviour
 {
+    static readonly ProfilerMarker ms_profilerMarker = new ProfilerMarker(ProfilerCategory.Scripts, "EntityWeaponBase.GetNearestBuildingAtRange");
+
     public abstract GameObject GetTarget();
     public abstract float GetMoveDistance();
 
@@ -20,17 +23,20 @@ public abstract class EntityWeaponBase : MonoBehaviour
 
     protected BuildingBase GetNearestBuildingAtRange(float range, Team team)
     {
-        if (BuildingList.instance == null)
-            return null;
+        using (ms_profilerMarker.Auto())
+        {
+            if (BuildingList.instance == null)
+                return null;
 
-        var building = BuildingList.instance.GetNearestBuilding(transform.position, team, AliveType.Alive);
-        if (building == null)
-            return null;
+            var building = BuildingList.instance.GetNearestBuilding(transform.position, team, AliveType.Alive);
+            if (building == null)
+                return null;
 
-        float dist = (building.GetGroundCenter() - transform.position).sqrMagnitude;
-        if (dist <= range * range)
-            return building;
-        return null;
+            float dist = (building.GetGroundCenter() - transform.position).sqrMagnitude;
+            if (dist <= range * range)
+                return building;
+            return null;
+        }
     }
 }
 
