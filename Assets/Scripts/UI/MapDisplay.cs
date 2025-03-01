eca2d572c74df0f762702b90672c9fe71d970058
Nico <nicolas-donald.laurent@laposte.net>
2025-03-01 22:21:27 +0100

    Minimap camera rect

--- Assets/Scripts/UI/MapDisplay.cs
+++ Assets/Scripts/UI/MapDisplay.cs
@@ -11,6 +11,7 @@ public class MapDisplay : MonoBehaviour
     [SerializeField] Image m_renderImage;
     [SerializeField] Material m_mapMaterial;
     [SerializeField] float m_rotationOffset;
+    [SerializeField] RectTransform m_cameraRect;
 
     SubscriberList m_subscriberList = new SubscriberList();
 
@@ -38,6 +39,13 @@ public class MapDisplay : MonoBehaviour
 
         Event<GetCameraRotationEvent>.Broadcast(e);
         transform.localRotation = Quaternion.Euler(0, 0, e.rotation + m_rotationOffset);
+
+        Vector2 pos1, pos2, pos3, pos4;
+        GetCameraSeeRect(out pos1, out pos2, out pos3, out pos4);
+        Rect camRect;
+        float camAngle;
+        PointsToNormalizedOBB(pos1, pos2, pos3, pos4, out camRect, out camAngle);
+        SetCameraRect(camRect, camAngle);
     }
 
     void OnLoadEnd(GenerationFinishedEvent e)
@@ -90,5 +98,94 @@ public class MapDisplay : MonoBehaviour
     void SetVisibility(bool visible)
     {
         gameObject.SetActive(visible);
+    }
+
+    void GetCameraSeeRect(out Vector2 pos1, out Vector2 pos2, out Vector2 pos3, out Vector2 pos4)
+    {
+        float h = 0;
+        if(BuildingList.instance != null)
+        {
+            var tower = BuildingList.instance.GetFirstBuilding(BuildingType.Tower);
+            if (tower != null)
+                h = tower.GetGroundCenter().y;
+        }
+
+        var plane = new Plane(Vector3.up, new Vector3(0, h, 0));
+
+        var camera = new GetCameraEvent();
+        Event<GetCameraEvent>.Broadcast(camera);
+
+        pos1 = Vector2.zero;
+        pos2 = Vector2.zero;
+        pos3 = Vector2.zero;
+        pos4 = Vector2.zero;
+
+        if (camera.camera == null)
+            return;
+
+        var width = Screen.width;
+        var height = Screen.height;
+
+        var ray1 = camera.camera.ScreenPointToRay(new Vector3(0, 0, 0));
+        var ray2 = camera.camera.ScreenPointToRay(new Vector3(width, 0, 0));
+        var ray3 = camera.camera.ScreenPointToRay(new Vector3(width, height, 0));
+        var ray4 = camera.camera.ScreenPointToRay(new Vector3(0, height, 0));
+
+        float enter = 0;
+        plane.Raycast(ray1, out enter);
+        var hitPoint = ray1.GetPoint(enter);
+        pos1 = new Vector2(hitPoint.x, hitPoint.z);
+
+        plane.Raycast(ray2, out enter);
+        hitPoint = ray2.GetPoint(enter);
+        pos2 = new Vector2(hitPoint.x, hitPoint.z);
+
+        plane.Raycast(ray3, out enter);
+        hitPoint = ray3.GetPoint(enter);
+        pos3 = new Vector2(hitPoint.x, hitPoint.z);
+
+        plane.Raycast(ray4, out enter);
+        hitPoint = ray4.GetPoint(enter);
+        pos4 = new Vector2(hitPoint.x, hitPoint.z);
+    }
+
+    void PointsToNormalizedOBB(Vector2 pos1, Vector2 pos2, Vector2 pos3, Vector2 pos4, out Rect rect, out float angle)
+    {
+        var left = (pos1 + pos2) / 2;
+        var right = (pos3 + pos4) / 2;
+        var top = (pos2 + pos3) / 2;
+        var bottom = (pos4 + pos1) / 2;
+
+        float width = (left - right).magnitude;
+        float height = (top - bottom).magnitude;
+
+        var center = (pos1 + pos2 + pos3 + pos4) / 4;
+
+        rect = new Rect(center - new Vector2(width, height) / 2, new Vector2(width, height));
+
+        angle = Vector2.SignedAngle(new Vector2(1, 0), (left - right));
+    }
+
+    void SetCameraRect(Rect rect, float angle)
+    {
+        if (m_cameraRect == null)
+            return;
+
+        m_cameraRect.localRotation = Quaternion.Euler(0, 0, angle);
+
+        GetGridEvent grid = new GetGridEvent();
+        Event<GetGridEvent>.Broadcast(grid);
+        if (grid.grid == null)
+            return;
+
+        int gridSize = GridEx.GetRealSize(grid.grid);
+
+        var normalizedRect = new Rect(rect.position / gridSize, rect.size / gridSize);
+
+        m_cameraRect.anchorMin = normalizedRect.position;
+        m_cameraRect.anchorMax = normalizedRect.position + normalizedRect.size;
+
+        m_cameraRect.offsetMin = Vector2.zero;
+        m_cameraRect.offsetMax = Vector2.zero;
     }
 }
