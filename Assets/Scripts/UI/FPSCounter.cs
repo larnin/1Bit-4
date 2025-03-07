dd6959b2b7c793b8fc1a48da399e3112d5a34de8
Nico <nicolas-donald.laurent@laposte.net>
2025-03-07 23:53:16 +0100

    Small balancing & fps counter

---
+++ Assets/Scripts/UI/FPSCounter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using TMPro;
+
+public class FPSCounter : MonoBehaviour
+{
+    TMP_Text m_text;
+
+    float lastFPS = 0;
+
+    private void Awake()
+    {
+        m_text = GetComponent<TMP_Text>();
+        if (m_text == null)
+            Destroy(gameObject);
+    }
+
+    private void Update()
+    {
+        float fps = 1 / Time.deltaTime;
+
+        if (lastFPS == 0)
+            lastFPS = fps;
+
+        const float percent = 0.1f;
+
+        lastFPS = lastFPS * (1 - percent) + fps * percent;
+
+        m_text.text = Mathf.RoundToInt(lastFPS).ToString();
+    }
+}
